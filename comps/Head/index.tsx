b27046a4cb4a1b9dff75c727eb117291cb8fff5a
Muhammad Alfan Jauhari <hi.alfanj@gmail.com>
2020-09-18 21:41:27 +0700

    Very Minor Change - Update Default Open Graph

--- comps/Head/index.tsx
+++ comps/Head/index.tsx
@@ -24,14 +24,14 @@ const Head = ({ title, description, keywords, canonical, image, type, children }
       <meta name="twitter:url" content={canonical ? canonical : config.url} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description ? description : config.description} />
-      <meta name="twitter:image" content={image ? `${config.url}thumbnails/${image}` : `${config.url}thumbnails/${config.thumbnail}`} />
+      <meta name="twitter:image" content={image ? `${config.url}thumbnails/${image}` : `${config.url}${config.thumbnail}`} />
       <meta name="twitter:creator" content={config.author} />
       <meta property="og:type" content={type ? type : 'website'} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description ? description : config.description} />
       <meta property="og:site_name" content={config.title} />
       <meta property="og:url" content={canonical ? canonical : config.url} />
-      <meta property="og:image" content={image ? `${config.url}thumbnails/${image}` : `${config.url}thumbnails/${config.thumbnail}`} />
+      <meta property="og:image" content={image ? `${config.url}thumbnails/${image}` : `${config.url}${config.thumbnail}`} />
       {children}
 		</NextHead>
 	)
